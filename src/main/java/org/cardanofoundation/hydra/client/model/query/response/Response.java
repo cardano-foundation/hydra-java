82f0e8b52669aa820bf9f5ed3e656fb00920585e
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-03-03 16:49:03 +0100

    generated getter

--- src/main/java/org/cardanofoundation/hydra/client/model/query/response/Response.java
+++ src/main/java/org/cardanofoundation/hydra/client/model/query/response/Response.java
@@ -1,17 +1,15 @@
 package org.cardanofoundation.hydra.client.model.query.response;
 
+import lombok.Getter;
 import org.cardanofoundation.hydra.client.model.Tag;
 
+@Getter
 public class Response {
 
     protected final Tag tag;
 
     protected Response(Tag tag) {
         this.tag = tag;
-    }
-
-    public Tag getTag() {
-        return tag;
     }
 
     @Override
