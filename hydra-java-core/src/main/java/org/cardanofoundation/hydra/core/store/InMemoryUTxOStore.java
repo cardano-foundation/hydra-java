d8e83744802f776e1e9eb4d163495b004989eef0
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-06-01 10:30:15 +0200

    feature: added additional explicit constructor for InMemoryUTxOStore

--- hydra-java-core/src/main/java/org/cardanofoundation/hydra/core/store/InMemoryUTxOStore.java
+++ hydra-java-core/src/main/java/org/cardanofoundation/hydra/core/store/InMemoryUTxOStore.java
@@ -1,14 +1,20 @@
 package org.cardanofoundation.hydra.core.store;
 
+import lombok.NoArgsConstructor;
 import org.cardanofoundation.hydra.core.model.UTXO;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
+@NoArgsConstructor
 public class InMemoryUTxOStore implements UTxOStore {
 
     private final AtomicReference<Map<String, UTXO>> reference = new AtomicReference<>(new HashMap<>());
+
+    public InMemoryUTxOStore(Map<String, UTXO> utxoMap) {
+        reference.set(utxoMap);
+    }
 
     @Override
     public Map<String, UTXO> getLatestUTxO() {
